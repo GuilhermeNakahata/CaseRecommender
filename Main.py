b60cae5d360acb6e609561457d7a1bf15caa5f66
Guilherme Nakahata <guilhermenakahata@gmail.com>
2021-02-17 14:49:52 -0300

    [GLHEMEH] Automatic Cross validation - ItemKNN e MostPopular

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
 from caserec.recommenders.rating_prediction.itemknn import ItemKNN
+from caserec.recommenders.rating_prediction.most_popular import MostPopular
 from caserec.utils.cross_validation import CrossValidation
 
 db = 'u.data'
 folds_path = ''
 
 recommender = ItemKNN()
+CrossValidation(input_file=db, recommender=recommender, dir_folds=folds_path, header=1, k_folds=5).compute()
+
+recommender = MostPopular()
 CrossValidation(input_file=db, recommender=recommender, dir_folds=folds_path, header=1, k_folds=5).compute()
