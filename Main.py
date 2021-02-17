62ebb10e6b0b9c02bd057d03e4b8f6e2a70d7c93
Guilherme Nakahata <guilhermenakahata@gmail.com>
2021-02-17 14:52:24 -0300

    [GLHEMEH] Implementa métricas.

--- Main.py
+++ Main.py
@@ -5,8 +5,10 @@ from caserec.utils.cross_validation import CrossValidation
 db = 'u.data'
 folds_path = ''
 
+metrics = ('PREC', 'RECALL', 'NDCG', 'MAP')
+
 recommender = ItemKNN()
-CrossValidation(input_file=db, recommender=recommender, dir_folds=folds_path, header=1, k_folds=5).compute()
+CrossValidation(input_file=db, recommender=recommender, dir_folds=folds_path, header=1, k_folds=5).compute(metrics = metrics)
 
 recommender = MostPopular()
-CrossValidation(input_file=db, recommender=recommender, dir_folds=folds_path, header=1, k_folds=5).compute()
+CrossValidation(input_file=db, recommender=recommender, dir_folds=folds_path, header=1, k_folds=5).compute(metrics = metrics)
